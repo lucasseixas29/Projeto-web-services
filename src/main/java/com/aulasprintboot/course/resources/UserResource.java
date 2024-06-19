faf24cbaf0bfabc48cef478e09ae40e38615ff40
Lucas Seixas <lucasseixas29@hotmail.com>
2024-06-18 23:08:05 -0300

    project created

---
+++ src/main/java/com/aulasprintboot/course/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.aulasprintboot.course.resources;
+
+
+import com.aulasprintboot.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+
+        User u = new User(1, "Maria", "maria@email.com", "99999999", "12345");
+
+        return ResponseEntity.ok().body(u);
+
+
+    }
+
+
+}
