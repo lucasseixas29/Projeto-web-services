a78f9adea62a5e6a3af74c3f5a2de246ece242b5
Lucas Seixas <lucasseixas29@hotmail.com>
2024-06-19 22:21:57 -0300

    OrderStatus enum

--- src/main/java/com/aulasprintboot/course/entities/Order.java
+++ src/main/java/com/aulasprintboot/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.aulasprintboot.course.entities;
 
+import com.aulasprintboot.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -19,6 +20,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -27,10 +31,11 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -49,6 +54,16 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
@@ -56,6 +71,7 @@ public class Order implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
+
 
     @Override
     public boolean equals(Object o) {
