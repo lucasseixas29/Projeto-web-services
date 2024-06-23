2f551ba3a60284363d7c91ac31f76373c6597335
Lucas Seixas <lucasseixas29@hotmail.com>
2024-06-22 23:14:45 -0300

    user insert

--- src/main/java/com/aulasprintboot/course/resources/UserResource.java
+++ src/main/java/com/aulasprintboot/course/resources/UserResource.java
@@ -4,12 +4,12 @@ package com.aulasprintboot.course.resources;
 import com.aulasprintboot.course.entities.User;
 import com.aulasprintboot.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,6 +32,13 @@ public class UserResource {
         User obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
--- src/main/java/com/aulasprintboot/course/services/UserService.java
+++ src/main/java/com/aulasprintboot/course/services/UserService.java
@@ -19,8 +19,11 @@ public class UserService {
     }
 
     public User findById(Long id) {
-         Optional<User> obj = userRepository.findById(id);
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
 
-         return obj.get();
+    public User insert(User obj) {
+        return userRepository.save(obj);
     }
 }
